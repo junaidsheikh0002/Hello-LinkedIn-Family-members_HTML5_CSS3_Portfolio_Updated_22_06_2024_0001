4645bf4dbf071adf24c4b498ff09c7180bf304cb
Junaid Ul Haque sheikh <156045075+junaidsheikh0002@users.noreply.github.com>
2024-06-22 14:05:59 +0500

    https://github.com/junaidsheikh0002/Hello-LinkedIn-Family-members_HTML5_CSS3_Portfolio_Updated_22_06_2024_0001/upload
    https://github.com/junaidsheikh0002/Hello-LinkedIn-Family-members_HTML5_CSS3_Portfolio_Updated_22_06_2024_0001/upload
    https://github.com/junaidsheikh0002/Hello-LinkedIn-Family-members_HTML5_CSS3_Portfolio_Updated_22_06_2024_0001/upload
    https://github.com/junaidsheikh0002/Hello-LinkedIn-Family-members_HTML5_CSS3_Portfolio_Updated_22_06_2024_0001/upload

---
+++ style.cpp
@@ -0,0 +1,62 @@
+body {
+    margin: 0;
+    font-family: Arial, sans-serif;
+    background-color: #0e0b16;
+    color: #ffffff;
+}
+
+header {
+    background-color: #15102c;
+    padding: 1rem;
+    text-align: center;
+}
+
+nav ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    display: flex;
+    justify-content: center;
+}
+
+nav ul li {
+    margin: 0 1rem;
+}
+
+nav ul li a {
+    color: #ffffff;
+    text-decoration: none;
+    font-weight: bold;
+}
+
+nav ul li a:hover {
+    text-decoration: underline;
+}
+
+section#home {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    text-align: center;
+}
+
+.intro h1 {
+    font-size: 3rem;
+    margin-bottom: 0.5rem;
+}
+
+.intro p {
+    font-size: 1.5rem;
+}
+
+.highlight {
+    color: #7f7fff;
+}
+
+.intro img {
+    margin-top: 2rem;
+    width: 300px;
+    height: auto;
+}
